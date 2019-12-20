0dfa5c3c5e621e5079c35c289bd0fe0a03fe9f99
Justice-love <eddyxu1213@126.com>
2019-12-20 16:45:02 +0800

    输入ip

---
+++ app/src/main/java/org/eddy/car/EnterActivity.java
@@ -0,0 +1,19 @@
+package org.eddy.car;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
--- app/src/main/java/org/eddy/car/MainActivity.java
+++ app/src/main/java/org/eddy/car/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
