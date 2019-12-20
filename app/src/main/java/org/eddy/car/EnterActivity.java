5961a9d059f39fe8d3185cfc909237d476d8592f
Justice-love <eddyxu1213@126.com>
2019-12-20 17:21:31 +0800

    发送指令

--- app/src/main/java/org/eddy/car/EnterActivity.java
+++ app/src/main/java/org/eddy/car/EnterActivity.java
@@ -1,10 +1,19 @@
 package org.eddy.car;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class EnterActivity extends AppCompatActivity {
+
+    private static final Pattern pattern = Pattern.compile("([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}");
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +23,21 @@ public class EnterActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        findViewById(R.id.ipConfirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                TextView ipView = findViewById(R.id.ipView);
+                String ip = ipView.getText().toString();
+                boolean flag = pattern.matcher(ip).matches();
+                if (! flag) {
+                    Toast.makeText(EnterActivity.this, "IP输入错误，请检查", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Intent intent = new Intent(EnterActivity.this, MainActivity.class);
+                intent.putExtra("ip", ip);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/org/eddy/car/MainActivity.java
+++ app/src/main/java/org/eddy/car/MainActivity.java
@@ -1,10 +1,15 @@
 package org.eddy.car;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Toast;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.eddy.car.proto.CarControlRequest;
+import org.eddy.car.proto.CarRpcGrpc;
+import org.eddy.car.proto.Direction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
             "</body>\n" +
             "\n" +
             "</html>";
+
+    private static CarRpcGrpc.CarRpcBlockingStub stub;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String ip = getIntent().getStringExtra("ip");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(ip, 18901).usePlaintext().build();
+        stub = CarRpcGrpc.newBlockingStub(channel);
     }
+
 
     @Override
     protected void onStart() {
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "向前", Toast.LENGTH_LONG).show();
+                stub.commond(CarControlRequest.newBuilder().setDirection(Direction.FRONT).build());
             }
         });
 
@@ -39,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "向后", Toast.LENGTH_LONG).show();
+                stub.commond(CarControlRequest.newBuilder().setDirection(Direction.BACK).build());
             }
         });
 
@@ -46,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "向右", Toast.LENGTH_LONG).show();
+                stub.commond(CarControlRequest.newBuilder().setDirection(Direction.RIGHT).build());
             }
         });
 
@@ -53,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "向左", Toast.LENGTH_LONG).show();
+                stub.commond(CarControlRequest.newBuilder().setDirection(Direction.LEFT).build());
             }
         });
     }
